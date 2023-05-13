0da807a548ca951239f4b6f26803455df33bfc2c
thanhduong001 <thanhvietduong@gmail.com>
2023-05-13 13:12:43 +0100

    Assessment 2
    new folder

--- panelmethod.m
+++ panelmethod.m
@@ -9,8 +9,8 @@ xmin = -2.5;
 ymax = 2;
 ymin = -2;
 del = 1.5;
-ga = 1;
-gb = 0;
+ga = 2;
+gb = 1;
 psi = zeros(nx, ny);
 for k = [1:1:nv]
     Gamma(k) =  (ga + (k-1)*(gb-ga)/(nv-1))*del/nv;
@@ -20,7 +20,7 @@ for k = [1:1:nv]
             ym(i,j) = ymin + (j-1)*(ymax-ymin)/(ny-1);
             psi(i,j) = psi(i,j) + psipv(k*del/nv,0,Gamma(k),xm(i,j),ym(i,j));
         end
-    end
+        endwar
 end
 c = -0.15:0.05:0.15;
 contour(xm,ym,psi,c)
