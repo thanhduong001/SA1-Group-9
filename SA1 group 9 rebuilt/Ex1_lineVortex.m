fe553a2003d28eabd759e8a25aa6071772498892
thanhduong001 <thanhvietduong@gmail.com>
2023-05-15 13:52:25 +0100

    Rebuilt ex1

---
+++ SA1 group 9 rebuilt/Ex1_lineVortex.m
@@ -0,0 +1,13 @@
+clear
+close all
+
+xc = 0.75;
+yc = 0.5;
+Gamma = 3;
+nv = 1;
+
+[xm, ym] = buildSpace();
+psi = psipv(xc,yc,Gamma,xm,ym)
+
+c = -0.4:0.2:1.2
+contour(xm,ym,psi,c)
--- SA1 group 9 rebuilt/buildSpace.m
+++ SA1 group 9 rebuilt/buildSpace.m
@@ -1,4 +1,4 @@
-function [psi, xm, ym] = buildSpace(Gamma, nv, xc, yc)
+function [xm, ym] = buildSpace()
 
 nx = 51;
 ny = 41;
@@ -8,14 +8,10 @@ xmin = -2.5;
 ymax = 2;
 ymin = -2;
 
-for s = 1:1:nv
-    for i = 1:1:nx
-        for j = 1:1:ny
-            xm(i,j) = xmin + (i-1)*(xmax-xmin)/(nx-1);
-            ym(i,j) = ymin + (j-1)*(ymax-ymin)/(ny-1);
-            psi(i,j) = psipv(xc,yc,Gamma(s),xm(i,j),ym(i,j));
-        end
+for i = 1:1:nx
+    for j = 1:1:ny
+        xm(i,j) = xmin + (i-1)*(xmax-xmin)/(nx-1);
+        ym(i,j) = ymin + (j-1)*(ymax-ymin)/(ny-1);
     end
 end
-
 end
