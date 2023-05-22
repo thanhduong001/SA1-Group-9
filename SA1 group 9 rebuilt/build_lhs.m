0235a4efce29aadaaf36522011d85230927c31ee
thanhduong001 <thanhvietduong@gmail.com>
2023-05-22 11:31:07 +0100

    Cleanup

--- SA1 group 9 rebuilt/build_lhs.m
+++ SA1 group 9 rebuilt/build_lhs.m
@@ -12,22 +12,22 @@ INFB = circshift(INFB, [0 1]);
 psip = INFA + INFB;
 psip(np, :) = zeros(1, np);
 
-% lhsmat = circshift(psip, [0 1]) - psip;
-% lhsmat(1,:) = 0;
-% lhsmat(1,1) = 1;
-% lhsmat(np,:) = 0;
-% lhsmat(np, np) = 1;
+lhsmat = circshift(psip, [0 1]) - psip;
+lhsmat(1,:) = 0;
+lhsmat(1,1) = 1;
+lhsmat(np,:) = 0;
+lhsmat(np, np) = 1;
 
 % %%%%%%
 
 
-lhsmat = zeros(np,np);
-for i = [2:1:np-1]
-    for j = [1:1:np]
-        lhsmat(i,j)  = psip(i,j) - psip(i-1,j);
-    end
-end
-
-lhsmat(1,1) = 1;
-lhsmat(np, np) = 1;
+% lhsmat = zeros(np,np);
+% for i = [2:1:np-1]
+%     for j = [1:1:np]
+%         lhsmat(i,j)  = psip(i,j) - psip(i-1,j);
+%     end
+% end
+% 
+% lhsmat(1,1) = 1;
+% lhsmat(np, np) = 1;
 
