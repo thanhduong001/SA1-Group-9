4df4d5b8fc2b7f3f65b1ef6e4e7a312788c3f9d9
thanhduong001 <thanhvietduong@gmail.com>
2023-05-24 15:32:31 +0100

    CLean

--- SA1 Week 2 THIS ONE/ueintbit.m
+++
@@ -1,6 +0,0 @@
-function [f] = ueintbit(xa,ua,xb,ub)
-Re = 2500;
-uBar = (ua + ub)/2;
-uDelta = ub - ua;
-xDelta = xb - xa;
-f = (uBar ^ 5 + 5/6 * uBar ^ 3 * uDelta ^ 2 + 1/16 * uBar * uDelta ^ 4) * xDelta;
---
+++ SA1 Week 2 cleaned/Ex2_Varying_ue.m
@@ -0,0 +1,36 @@
+clear
+close all
+clc
+
+Re = 100000;
+n = 101;
+duedx = ones(1,n) * -1;
+ue0 = ones(1,n) * 1;
+x = (0:.01:1);
+ue = ue0 + duedx .* x;
+
+integral = zeros(1,n);
+for i = 2:n
+    integral(i) = integral(i-1) + ueintbit(x(i-1), ue(i-1), x(i), ue(i)); 
+end
+
+thetasq = zeros(1,n);
+theta = zeros(1,n);
+laminar = true;
+He = zeros(1,n);
+i = 1;
+while laminar && i < n
+    i = i + 1;
+    thetasq(i) = (0.45/Re)*ue(i)^-6 * integral(i);
+    theta(i) = thetasq(i) ^ 0.5;
+    m = -Re * (thetasq(i) * duedx(i));
+    H = thwaites_lookup(m);
+    He(i) = laminar_He(H);
+    Rethet = Re * ue(i) * theta(i);
+    if log(Rethet) >= 18.4*He - 21.74
+        laminar = false;
+        disp([x(i) Rethet/1000])
+    end
+end
+
+plot(x, theta);
